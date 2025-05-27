fe46f4dcd2369ac9b5da3de8f706b252fa949bf8
Gugu <chicken.nugger135@gmail.com>
2025-05-28 00:19:46 +0300

    v.1.2a
    Add audio and video format selection menus and update extraction commands

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -16,13 +16,15 @@ destPath=$(pwd)
 file=""
 quietMode=""
 simulate=""
+audioFormat="mp3"
+videoFormat="mp4"
 
 
 function continue()
 {
 	echo ""
 	echo -e "${White}"
-	read -p "		Press enter to continue..." temp
+	read -s -p "		Press enter to continue..." temp
 }
 
 function showCredits()
@@ -54,6 +56,7 @@ function showCredits()
 	continue
 }
 
+
 # Function that checks if all links in a given file are valid
 function validate_links()
 {
@@ -79,6 +82,209 @@ function validate_links()
 
 # Usage:
 #if validate_links "links.txt"; then
+
+function showAudioFormatMenu()
+{
+	clear
+	echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+	echo ""
+	echo -e "	[${Red}1${White}] aac\n	[${Red}2${White}] alac\n	[${Red}3${White}] flac\n	[${Red}4${White}] m4a\n	[${Red}5${White}] mp3 (Default)\n	[${Red}6${White}] opus\n	[${Red}7${White}] vorbis\n	[${Red}8${White}] wav\n\n	[${Red}9${White}] Back"
+	echo -e "${White}==================================="
+	echo ""
+	read -p "Select Option: " choice
+	case $choice in
+
+	1)
+		audioFormat="aac"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}aac${White}."
+		continue
+		showFormatMenu
+		;;
+	2)
+		audioFormat="alac"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}alac${White}."
+		continue
+		showFormatMenu
+		;;
+	3)
+		audioFormat="flac"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}flac${White}."
+		continue
+		showFormatMenu
+		;;
+	4)
+		audioFormat="m4a"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}m4a${White}."
+		continue
+		showFormatMenu
+		;;
+	5)
+		audioFormat="mp3"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}mp3${White}."
+		continue
+		showFormatMenu
+		;;
+	6)	
+		audioFormat="opus"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}opus${White}."
+		continue
+		showFormatMenu
+		;;
+	7)
+		audioFormat="vorbis"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}vorbis${White}."
+		continue
+		showFormatMenu
+		;;
+	8)
+		audioFormat="wav"
+		clear
+		echo -e "${White}========== ${Red} AUDIO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Audio format set to ${Red}wav${White}."
+		continue
+		showFormatMenu
+		;;
+	9)
+		showFormatMenu
+		;;
+	*)
+		showAudioFormatMenu
+		;;
+	esac
+}
+
+# Function that displays the video format menu. This is where users can set their preferred video format
+function showVideoFormatMenu()
+{
+	# Supported video formats: avi, flv, gif, mkv, mov, mp4, webm
+	clear
+	echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+	echo ""
+	echo -e "	[${Red}1${White}] avi\n	[${Red}2${White}] flv\n	[${Red}3${White}] gif\n	[${Red}4${White}] mkv\n	[${Red}5${White}] mov\n	[${Red}6${White}] mp4 (Default)\n	[${Red}7${White}] webm\n\n	[${Red}8${White}] Back"
+	echo -e "${White}==================================="
+	echo ""
+	read -p "Select Option: " choice
+	case $choice in
+
+	1)
+		videoFormat="avi"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}avi${White}."
+		continue
+		showFormatMenu
+		;;
+	2)
+		videoFormat="flv"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}flv${White}."
+		continue
+		showFormatMenu
+		;;
+	3)
+		videoFormat="gif"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}gif${White}."
+		continue
+		showFormatMenu
+		;;
+	4)
+		videoFormat="mkv"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}mkv${White}."
+		continue
+		showFormatMenu
+		;;
+	5)
+		videoFormat="mov"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}mov${White}."
+		continue
+		showFormatMenu
+		;;
+	6)
+		videoFormat="mp4"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}mp4${White}."
+		continue
+		showFormatMenu
+		;;
+	7)
+		videoFormat="webm"
+		clear
+		echo -e "${White}========== ${Red} VIDEO FORMAT${White} =========="
+		echo ""
+		echo -e "	${White}Video format set to ${Red}webm${White}."
+		continue
+		showFormatMenu
+		;;
+	8)
+		showFormatMenu
+		;;
+	*)
+		showVideoFormatMenu
+		;;
+	esac
+}
+
+function showFormatMenu()
+{
+	clear
+	echo -e "${White}========== ${Red} FORMAT OPTIONS${White} =========="
+	echo ""
+	echo -e "	[${Red}1${White}] Set Audio Format\n	[${Red}2${White}] Set Video Format\n\n	[${Red}3${White}] Back"
+	echo -e "${White}====================================="
+	echo ""
+	read -p "Select Option: " choice
+	case $choice in	
+		
+	1)
+		showAudioFormatMenu
+		;;
+	2)
+		showVideoFormatMenu
+		;;
+	3)
+		showMainMenu
+		;;
+	*)
+		showFormatMenu
+		;;
+	esac
+}
 
 # Function that displays the file options menu. This is where users must set their input file
 function showFileOptionsMenu()
@@ -346,7 +552,7 @@ function showMainMenu()
 	clear
 	echo -e "${White}============== ${Red}MAIN MENU ${White}=============="
 	echo ""
-	echo -e "	[${Red}1${White}] Audio Extract\n	[${Red}2${White}] Video Extract\n	[${Red}3${White}] Extract From File\n\n	[${Red}4${White}] Destination Path Settings\n\n	[${Red}5${White}] Internal Options\n\n	[${Red}6${White}] Credits\n	[${Red}99${White}] Exit"
+	echo -e "	[${Red}1${White}] Audio Extract\n	[${Red}2${White}] Video Extract\n	[${Red}3${White}] Extract From File\n\n	[${Red}4${White}] Destination Path Settings\n	[${Red}5${White}] Format Settings\n\n	[${Red}6${White}] Internal Options\n\n	[${Red}7${White}] Credits\n	[${Red}99${White}] Exit"
 	echo -e "${White}======================================="
 	echo ""
 	read -p "Select option: " choice
@@ -356,7 +562,7 @@ function showMainMenu()
 		clear
 		echo -e "${White}=============== ${Red}EXTRACT ${White}==============="
 		read -p "Enter link: " link
-		yt-dlp -x --audio-format mp3 -P $destPath -o '%(title)s.%(ext)s' $link $quietMode --no-warnings
+		yt-dlp -x --audio-format $audioFormat -P $destPath -o '%(title)s.%(ext)s' $link $quietMode --no-warnings
 		continue
 		showMainMenu
 		;;
@@ -364,7 +570,7 @@ function showMainMenu()
 		clear
 		echo -e "${White}=============== ${Red}EXTRACT ${White}==============="
 		read -p "Enter link: " link
-		yt-dlp -f bestvideo*+bestaudio/best --remux-video mp4 -P $destPath -o '%(title)s.%(ext)s' --no-warnings $link
+		yt-dlp -f bestvideo*+bestaudio/best --remux-video $videoFormat -P $destPath -o '%(title)s.%(ext)s' --no-warnings $link
 		continue
 		showMainMenu
 		;;
@@ -375,9 +581,12 @@ function showMainMenu()
 		showPathMenu
 		;;
 	5)
-		showInternalMenu
+		showFormatMenu
 		;;
 	6)
+		showInternalMenu
+		;;
+	7)
 		showCredits
 		showMainMenu
 		;;
